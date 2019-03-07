38edc3675f720f5a0bcd0c132ed28745b51f675a
Jake Bills <jakebills1@gmail.com>
2019-03-07 14:52:19 -0700

    built out controllers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,39 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :update, :destroy]
   def index
+    @books = Books.all
   end
 
   def show
   end
 
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      render json: @book
+    else
+      render_error(@book)
+    end
   end
 
   def update
+    if @book.update
+      render json: @book
+    else
+      render_error(@book)
+    end
   end
 
   def destroy
+    @book.destroy
+    render json: { message: "Book removed"}, status: :ok
   end
+
+  private
+    def set_book
+      @book = Book.find(params[:id])
+    end
+    def book_params
+      params.require(:book).permit(:name, :author, :genre)
+    end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,44 @@
 class ReviewsController < ApplicationController
+  before_action :set_book
+  before_action :set_review, only: [:show, :update, :destroy]
   def index
+    render json: @book.reviews
   end
 
   def show
+    render json: @review
   end
 
   def create
+    @review = @book.reviews.new(review_params)
+    if @review.save
+      render json: @review
+    else
+      render_error(@review)
+    end
   end
 
   def update
+    if @review.update(review_params)
+      render json: @review
+    else
+      render_error(@review)
+    end
   end
 
   def destroy
+    @review.destroy
+    render json: { message: "Review Removed"}, status: :ok
   end
+
+  private
+    def set_book
+      @book = Book.find(params[:book_id])
+    end
+    def set_review
+      @review = Review.find(params[:id])
+    end
+    def review_params
+      params.require(:review).permit(:stars, :body)
+    end
 end
